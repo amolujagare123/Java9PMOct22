f06815c40190a6862bbbbb4f084cbd82f150dce5
amolu <amolujagare@gmail.com>
2022-10-06 22:07:05 +0530

    java

---
+++ src/loops/ForDemo1.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class ForDemo1 {
+
+    public static void main(String[] args) {
+
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+
+    }
+}
--- src/loops/WhileDemo.java
+++ src/loops/WhileDemo.java
@@ -4,12 +4,12 @@ public class WhileDemo {
 
     public static void main(String[] args) {
 
-        int i =1;
+        int i =1; // initialization
 
-        while (i<=10)
+        while (i<=10) // condition checking
         {
             System.out.println(i);
-            i = i + 1 ;
+            i = i + 1 ; // increment
         }
 
 
